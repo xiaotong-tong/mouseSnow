bce4f7c95d16110ede6b217a3daa2adf22efa523
小恸恸 <wtt150249@gmail.com>
2024-01-04 21:07:44 +0800

    feat: 优化类型说明

--- src/index.d.ts
+++ src/index.d.ts
@@ -17,7 +17,16 @@ declare module "mousesnow" {
 		 * @param {string|HTMLElement} [options.container] - 容器，默认为 document
 		 * @param {number} [options.throttle] - 节流时间，默认为 60ms
 		 * @param {function():{x:number, y:number}} [options.velocity] - 雪花速度，默认为x在 -1 ~ 1 之间随机，y 为 1
+		 * @param {boolean|object} [options.downPointSnow] - 是否开启鼠标点击雪花特效，默认为 true，如果为 true，则使用默认配置，如果为 false，则不开启
+		 * @param {boolean} [options.downPointSnow.isDisabled] - 是否禁用，默认为 false
+		 * @param {number} [options.downPointSnow.num] - 雪花数量，默认为 20 个
+		 * @param {string} [options.downPointSnow.character] - 雪花字符，默认为 *
+		 * @param {string} [options.downPointSnow.size] - 大小，默认为 32px
+		 * @param {number} [options.downPointSnow.lifeSpan] - 生命周期，默认为 80 帧
+		 * @param {number} [options.downPointSnow.destoryScalePercent] - 雪花缩放倍率小于该值时销毁，默认为 0.3
+		 * @param {function():{x:number, y:number}} [options.downPointSnow.velocity] - 雪花速度，默认为 x 和 y 在 -2 ~ 2 之间随机
 		 */
+
 		constructor(options: {
 			color?: string | (() => string);
 			size?: string;
@@ -27,6 +36,17 @@ declare module "mousesnow" {
 			container?: string | HTMLElement;
 			throttle?: number;
 			velocity?: () => { x: number; y: number };
+			downPointSnow?:
+				| boolean
+				| {
+						isDisabled?: boolean;
+						num?: number;
+						character?: string;
+						size?: string;
+						lifeSpan?: number;
+						destoryScalePercent?: number;
+						velocity?: () => { x: number; y: number };
+				  };
 		});
 
 		/**
@@ -39,6 +59,14 @@ declare module "mousesnow" {
 		 * @property {string|HTMLElement} options.container - 容器
 		 * @property {number} options.throttle - 节流时间
 		 * @property {function():{x:number, y:number}} options.velocity - 雪花速度
+		 * @property {boolean|object} [options.downPointSnow] - 是否开启鼠标点击雪花特效，默认为 true，如果为 true，则使用默认配置，如果为 false，则不开启
+		 * @property {boolean} [options.downPointSnow.isDisabled] - 是否禁用，默认为 false
+		 * @property {number} [options.downPointSnow.num] - 雪花数量，默认为 20 个
+		 * @property {string} [options.downPointSnow.character] - 雪花字符，默认为 *
+		 * @property {string} [options.downPointSnow.size] - 大小，默认为 32px
+		 * @property {number} [options.downPointSnow.lifeSpan] - 生命周期，默认为 80 帧
+		 * @property {number} [options.downPointSnow.destoryScalePercent] - 雪花缩放倍率小于该值时销毁，默认为 0.3
+		 * @property {function():{x:number, y:number}} [options.downPointSnow.velocity] - 雪花速度，默认为 x 和 y 在 -2 ~ 2 之间随机
 		 */
 
 		get options(): {
@@ -50,6 +78,17 @@ declare module "mousesnow" {
 			container: string | HTMLElement;
 			throttle: number;
 			velocity: () => { x: number; y: number };
+			downPointSnow:
+				| boolean
+				| {
+						isDisabled?: boolean;
+						num?: number;
+						character?: string;
+						size?: string;
+						lifeSpan?: number;
+						destoryScalePercent?: number;
+						velocity?: () => { x: number; y: number };
+				  };
 		};
 	}
 }
--- src/index.js
+++ src/index.js
@@ -80,6 +80,14 @@ export default class MouseSnow {
 	 * @param {string|HTMLElement} [options.container] - 容器，默认为 document
 	 * @param {number} [options.throttle] - 节流时间，默认为 60ms
 	 * @param {function():{x:number, y:number}} [options.velocity] - 雪花速度，默认为x在 -1 ~ 1 之间随机，y 为 1
+	 * @param {boolean|object} [options.downPointSnow] - 是否开启鼠标点击雪花特效，默认为 true，如果为 true，则使用默认配置，如果为 false，则不开启
+	 * @param {boolean} [options.downPointSnow.isDisabled] - 是否禁用，默认为 false
+	 * @param {number} [options.downPointSnow.num] - 雪花数量，默认为 20 个
+	 * @param {string} [options.downPointSnow.character] - 雪花字符，默认为 *
+	 * @param {string} [options.downPointSnow.size] - 大小，默认为 32px
+	 * @param {number} [options.downPointSnow.lifeSpan] - 生命周期，默认为 80 帧
+	 * @param {number} [options.downPointSnow.destoryScalePercent] - 雪花缩放倍率小于该值时销毁，默认为 0.3
+	 * @param {function():{x:number, y:number}} [options.downPointSnow.velocity] - 雪花速度，默认为 x 和 y 在 -2 ~ 2 之间随机
 	 */
 	constructor(options) {
 		this.#options = handleOption(options);
@@ -87,7 +95,10 @@ export default class MouseSnow {
 		this.#bindEvents();
 
 		if (this.#options.throttle) {
-			this.#createAnimation = throttle(this.#originCreateAnimation.bind(this), this.#options.throttle);
+			this.#createAnimation = throttle(
+				this.#originCreateAnimation.bind(this),
+				this.#options.throttle
+			);
 		}
 	}
 
@@ -147,6 +158,14 @@ export default class MouseSnow {
 	 * @property {string|HTMLElement} options.container - 容器
 	 * @property {number} options.throttle - 节流时间
 	 * @property {function():{x:number, y:number}} options.velocity - 雪花速度
+	 * @property {boolean|object} [options.downPointSnow] - 是否开启鼠标点击雪花特效，默认为 true，如果为 true，则使用默认配置，如果为 false，则不开启
+	 * @property {boolean} [options.downPointSnow.isDisabled] - 是否禁用，默认为 false
+	 * @property {number} [options.downPointSnow.num] - 雪花数量，默认为 20 个
+	 * @property {string} [options.downPointSnow.character] - 雪花字符，默认为 *
+	 * @property {string} [options.downPointSnow.size] - 大小，默认为 32px
+	 * @property {number} [options.downPointSnow.lifeSpan] - 生命周期，默认为 80 帧
+	 * @property {number} [options.downPointSnow.destoryScalePercent] - 雪花缩放倍率小于该值时销毁，默认为 0.3
+	 * @property {function():{x:number, y:number}} [options.downPointSnow.velocity] - 雪花速度，默认为 x 和 y 在 -2 ~ 2 之间随机
 	 */
 
 	get options() {
